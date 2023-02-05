bf6a6e07dde6d5917a253f9558c72eae6b92972b
Bilal Sardar <BilalSardar@users.noreply.huggingface.co>
2023-02-05 09:52:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 import os
 os.system("pip install IPython")
 os.system("pip install tqdm")
-
+os.system("pip install -Iv https://pypi.python.org/packages/source/p/pyFluidSynth/pyFluidSynth-1.2.4.tar.gz#md5=60079310701d0b9298b65a8b6728ffcc")
 # for data
 os.system("pip install fuzzywuzzy[speedup]")
 
